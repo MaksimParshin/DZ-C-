3438d1f468a6d04626e10881d19f523936218bd3
MaksimParshin <mak.yp@yandex.ru>
2022-08-09 13:47:04 +0300

    solved 3 tasks

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+string str = number.ToString();
+char digit2 = str[1];
+string newStr = digit2.ToString();
+int newNumber = Convert.ToInt32(newStr);
+Console.WriteLine(newNumber);
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N > 99)
+{
+    string str1 = number.ToString();
+    char dig2 = str[1];
+    string newS = digit2.ToString();
+    int newN = Convert.ToInt32(newStr);
+    Console.WriteLine(newNumber);
+}
+else
+{
+     Console.WriteLine("третьей цифры нет");
+}
+
+//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n == 6 || N ==7)
+{
+    Console.WriteLine("Да");
+}
+else 
+{
+    Console.WriteLine("Нет");
+}
